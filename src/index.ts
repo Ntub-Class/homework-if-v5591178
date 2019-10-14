423492347d0a98f398ab59d97a6b122c50f02774
v5591178 <55239491+v5591178@users.noreply.github.com>
2019-10-14 20:07:33 +0800

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 // 請介紹兩個字串方法跟數字方法
+let arry1 = ['a','b','c'];
+let arry2 = ['d','e','f'];
+console.log(arry1.concat(arry2));
 
+let str = "Runoob Google"
+console.log(str.toLowerCase( ));
+
+var num = new Number(177.1234);
+console.log(num.toLocaleString());
+
+var num = new Number(10);
+console.log(num.valueOf());
 
 // 錢錢大於一個值，叫乾爹
 let money = 40000;
 let limit = 500000; // 限制值
+
+//if(money > 800000){
+//    console.log('不可能');
+//}else{
+//    console.log('叫乾爹')
+//}
+if (money > 800000) {
+    console.log('不可能')        
+} else {
+    console.log('叫乾爹')    
+}
 
